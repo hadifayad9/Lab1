fa62914c677a91a0d0ee8f9b49bc7b571cf7547c
hadifayad9 <hadifayad9@hotmail.com>
2021-10-05 21:18:36 -0400

    Changed from dev to deployment

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -20,12 +20,10 @@ namespace Lab1
 
             var configuration = host.Services.GetService<IConfiguration>();
             var hosting = host.Services.GetService<IWebHostEnvironment>();
-            if (hosting.IsDevelopment())
-            {
-                var secrets = configuration.GetSection("Secrets").Get<AppSecrets>();
-                DbInitializer.appSecrets = secrets;
-            }
 
+            var secrets = configuration.GetSection("Secrets").Get<AppSecrets>();
+            DbInitializer.appSecrets = secrets;
+        
             using (var scope = host.Services.CreateScope())
             {
                 DbInitializer.SeedUsersAndRoles(scope.ServiceProvider).Wait();
